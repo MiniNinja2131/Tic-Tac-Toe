a3583bd245e2c4d9030c1e7ffd251d569d38d749
unknown <dannyche@live.co.uk>
2022-08-05 00:03:50 +0100

    Tidying up code

--- player.py
+++ player.py
@@ -9,16 +9,7 @@ class Player:
     # Allow all players to get their next move given the current state of the game
     def getMove(self, game):
         pass
-
-class RandomComputerPlayer(Player):
-    def __init__(self, letter):
-        super().__init__(letter)
-
-    def getMove(self, game):
-        # Computer will randomly choose an valid spot for their next move
-        square = random.choice(game.availableMove())
-        return square
-
+    
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
@@ -35,7 +26,7 @@ class HumanPlayer(Player):
             # 2) if that spot is not available on the board, we can also say its invalid
             try:
                 val = int(square)
-                if val not in game.availableMove():#
+                if val not in game.availableMove():
                     raise ValueError
                 # If both validation check passes then its an valid move
                 validSquare = True
